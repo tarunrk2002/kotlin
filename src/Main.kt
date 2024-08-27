1266483a65411f95dfd3c78fb52fce84110c4677
tarun <tarunrk2002@gmail.com>
2024-08-27 13:19:35 +0530

    third

--- src/Main.kt
+++ src/Main.kt
@@ -5,11 +5,20 @@ fun main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
     println("Hello, " + name + "!")
-    println("jfwfioew")
+    print("jfwfioew")
 
     for (i in 1..5) {
         //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
         // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
         println("i = $i")
+
+        aa()
     }
+
+
+}
+
+fun aa()
+{
+   print("D")
 }
